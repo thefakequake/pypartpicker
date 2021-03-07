bcb8560d90997ebe36828e7f707df3476e3f7708
lukadd <lukadd@icloud.com>
2021-03-07 18:27:21 -0500

    feat: Updated RegEx to support Saved User Lists

--- pypartpicker/regex.py
+++ pypartpicker/regex.py
@@ -2,10 +2,10 @@ import re
 
 
 def get_list_links(string):
-    list_regex = re.compile("((?:http|https)://(?:[a-z]{2}.pcpartpicker|pcpartpicker).com/list/(?:[a-zA-Z0-9]{6}))")
+    list_regex = re.compile("((?:http|https)://(?:[a-z]{2}.)?pcpartpicker.com/(?:(?:list/(?:[a-zA-Z0-9]{6}))|(?:user/(?:[\\w]+)/saved/(?:[a-zA-Z0-9]{6}))))")
     return re.findall(list_regex, string)
 
 
 def get_product_links(string):
-    product_regex = re.compile("((?:http|https)://(?:[a-z]{2}.pcpartpicker|pcpartpicker).com/product/(?:[a-zA-Z0-9]{6}))")
+    product_regex = re.compile("((?:http|https)://(?:[a-z]{2}.)?pcpartpicker.com/product/(?:[a-zA-Z0-9]{6}))")
     return re.findall(product_regex, string)
