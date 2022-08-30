ebfaabb8b609edbb54ac6d0066e6621ab4020e68
QuaKe <67868381+quakecodes@users.noreply.github.com>
2022-08-30 22:13:06 +0100

    Format regex.py

--- pypartpicker/regex.py
+++ pypartpicker/regex.py
@@ -1,7 +1,11 @@
 import re
 
-LIST_REGEX = re.compile("((?:http|https)://(?:[a-z]{2}.)?pcpartpicker.com/(?:(?:list/(?:[a-zA-Z0-9]{6}))|(?:user/(?:[\\w]+)/saved/(?:[a-zA-Z0-9]{6}))))")
-PRODUCT_REGEX = re.compile("((?:http|https)://(?:[a-z]{2}.)?pcpartpicker.com/product/(?:[a-zA-Z0-9]{6}))")
+LIST_REGEX = re.compile(
+    "((?:http|https)://(?:[a-z]{2}.)?pcpartpicker.com/(?:(?:list/(?:[a-zA-Z0-9]{6}))|(?:user/(?:[\\w]+)/saved/(?:[a-zA-Z0-9]{6}))))"
+)
+PRODUCT_REGEX = re.compile(
+    "((?:http|https)://(?:[a-z]{2}.)?pcpartpicker.com/product/(?:[a-zA-Z0-9]{6}))"
+)
 
 
 def get_list_links(string):
