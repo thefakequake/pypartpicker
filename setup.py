ea3925d2640929d214020e6184c4a21679deeb50
QuaKe <67868381+quakecodes@users.noreply.github.com>
2022-09-02 16:45:59 +0100

    Add 3.10 to classifiers, bump version and remove old email

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
-setup(name='pypartpicker',
-      version='1.9.1',
-      description='A package that scrapes pcpartpicker.com and returns the results as objects.',
-      packages=['pypartpicker'],
-      url='https://github.com/thefakequake/pypartpicker',
-      keywords = ['pcpartpicker', 'scraper', 'list', 'beautifulsoup', 'pc', 'parts'],
-      author_email='mastermind4560@gmail.com',
-      install_requires=['bs4', 'requests'],
-      zip_safe=False,
-      download_url = "https://github.com/thefakequake/pypartpicker/archive/refs/tags/v1.9.1.tar.gz",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-        ])
-
-
+setup(
+    name="pypartpicker",
+    version="1.9.2",
+    description="A package that scrapes pcpartpicker.com and returns the results as objects.",
+    packages=["pypartpicker"],
+    url="https://github.com/thefakequake/pypartpicker",
+    keywords=["pcpartpicker", "scraper", "list", "beautifulsoup", "pc", "parts"],
+    install_requires=["bs4", "requests"],
+    zip_safe=False,
+    download_url="https://github.com/thefakequake/pypartpicker/archive/refs/tags/v1.9.2.tar.gz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
