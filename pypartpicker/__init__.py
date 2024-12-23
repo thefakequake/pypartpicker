c19980593d85943d190fd23b781014e9a8b9207f
QuaKe <mastermind4560@gmail.com>
2024-12-23 09:12:40 +0000

    Add deprecation warning

--- pypartpicker/__init__.py
+++ pypartpicker/__init__.py
@@ -1,2 +1,9 @@
 from .part import *
 from .client import *
+
+
+class Scraper:
+    def __init__(self):
+        raise Exception(
+            "Initialising the library via Scraper is deprecated, use Client instead.\nRead 2.0 changes here: https://github.com/thefakequake/pypartpicker/tree/2.0"
+        )
